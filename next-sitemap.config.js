cbacad28c01135f01725790e6766f35b6dfebb5d
Gabriel Kochem <contato@gabrielkochem.adv.br>
2026-05-06 22:34:40 -0300

    chore: exclude login and dashboard from sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,14 @@
 module.exports = {
   siteUrl: 'https://www.gabrielkochem.adv.br',
   generateRobotsTxt: true,
+  exclude: ['/login', '/dashboard', '/dashboard/*'],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/login', '/dashboard', '/dashboard/*'],
+      },
+    ],
+  },
 }
